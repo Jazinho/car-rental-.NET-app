9feeb20a9500d6c587d7c919d583c36e3db22b40
Piotrek <piotrek4464@wp.pl>
2017-05-23 00:56:50 +0200

    Fix add backend setup and add modeland repos

---
+++ CarRentalBackendApp/CarRentalBackendApp/Repositories/CarRepository.cs
@@ -0,0 +1,47 @@
+﻿using CarRentalBackendApp.DAL;
+using CarRentalBackendApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalBackendApp.Repositories
+{
+    class CarRepository : ICarRepository
+    {
+        CarContext db = new CarContext();
+
+        public List<Car> GetAll()
+        {
+            return db.Cars.ToList();
+        }
+
+        public Car Get(int Id)
+        {
+            return db.Cars.Find(Id);
+        }
+
+        public int Create(Car Car)
+        {
+            var Created = db.Cars.Add(Car);
+            db.SaveChanges();
+
+            return Created.Id;
+        }
+
+        public Car Update(Car Car)
+        {
+            db.Entry(Car).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return Car;
+        }
+
+        public void Delete(int Id)
+        {
+            var Deleted = db.Cars.Find(Id);
+            db.Cars.Remove(Deleted);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ CarRentalBackendApp/CarRentalBackendApp/Repositories/IClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using CarRentalBackendApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalBackendApp.Repositories
+{
+    public interface IClientRepository
+    {
+        List<Client> GetAll();
+        int Create(Client Client);
+        Client Get(int Id);
+        Client Update(Client Client);
+        bool Delete(int Id);
+    }
+}
