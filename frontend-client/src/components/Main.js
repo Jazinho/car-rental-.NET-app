133bf4583a1e9a66e87e13f20879f3254976275b
jan <jan.palucki@gmail.com>
2017-05-22 22:53:27 +0200

    Set application background-image

--- frontend-client/src/components/Main.js
+++ frontend-client/src/components/Main.js
@@ -3,13 +3,11 @@ require('styles/App.css');
 
 import React from 'react';
 
-let backgroundImage = require('../images/background.jpg');
-
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={backgroundImage} alt="backgroundImage" />
+        Main Menu will be here<br/>
       </div>
     );
   }
