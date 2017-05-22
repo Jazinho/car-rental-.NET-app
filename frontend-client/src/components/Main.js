91b37b2f655ab3bfcaf18a56fb43b0fbc709ae51
jan <jan.palucki@gmail.com>
2017-05-22 22:27:59 +0200

    Added displaying background image

--- frontend-client/src/components/Main.js
+++ frontend-client/src/components/Main.js
@@ -3,14 +3,13 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+let backgroundImage = require('../images/background.jpg');
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <img src={backgroundImage} alt="backgroundImage" />
       </div>
     );
   }
